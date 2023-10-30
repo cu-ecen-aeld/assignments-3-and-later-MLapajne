9c4b537d48f6146919474b80c6983a2eb7806528
MLapajne <sivar.lapajne@gmail.com>
2023-10-30 06:38:01 -0400

    final-8.19

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,19 +1,19 @@
-#! /bin/sh
-AESDSOCKET_ARGS="$AESDSOCKET_ARGS -d"
+#!/bin/sh
+
+#DIR_PATH=$(dirname $(readlink -f "${BASH_SOURCE:-$0}"))
 
 case "$1" in
-    start)
-        echo "Starting aesdsocket"
-        start-stop-daemon -S -n aesdsocket -- /usr/bin/aesdsocket \
-        -- $AESDSOCKET_ARGS
+  start)
+        echo "Starting daemon: aesdsocket"
+		start-stop-daemon --start --exec /usr/bin/aesdsocket -- -d 
+        echo "Started daemon: aesdsocket"
         ;;
-    stop)
-        echo "Stopping aesdsocket"
-        start-stop-daemon -K -n aesdsocket
+  stop)
+        echo "Stopping daemon: aesdsocket"
+		start-stop-daemon --stop --exec /usr/bin/aesdsocket 
+        echo "Stopped daemon: aesdsocket"
         ;;
-    *)
-        echo "Usage: $0 {start|stop}"
+  *)
+        echo "Usage: "$0" {start|stop}"
         exit 1
 esac
-
-exit 0
