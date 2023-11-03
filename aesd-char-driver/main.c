f597a38782a28e0d8bb9d8fc5c6b56057c7b8b10
MLapajne <sivar.lapajne@gmail.com>
2023-11-03 09:51:24 -0400

    final-9.4

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -182,7 +182,7 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
     retval = count;
 
 out:
-    mutex_unlock(&pdev>lock);
+    mutex_unlock(&pdev->lock);
     return retval;
 }
 
