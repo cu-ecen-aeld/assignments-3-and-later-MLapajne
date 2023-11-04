8faca6445b3d176e322d8680b195e01324142c36
MLapajne <sivar.lapajne@gmail.com>
2023-11-04 11:33:42 -0400

    final-9.12

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -67,8 +67,10 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 */
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
+    const char * buffer_return = NULL;
     if (buffer->full)
     {
+      buffer_return = buffer->entry[buffer->out_offs].buffptr;
       buffer->out_offs++;
       if (buffer->out_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED)
         buffer->out_offs = 0;
@@ -87,6 +89,7 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     {
       buffer->full = true;
     }
+    return buffer_return;
 }
 
 long aesd_circular_buffer_find_offset_for_entry_index(struct aesd_circular_buffer *buffer, unsigned int entry_index)
